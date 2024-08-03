4302746e2f61bd89040146a31370a52d88cc4699
branfts <support@june07.com>
2024-08-03 12:01:43 -0700

    Updated by A10x Service

---
+++ public/u/frontroworbust.js
@@ -0,0 +1,13 @@
+
+const links = [
+    { url: 'https://youtube.com/@FrontRowOrBust' },
+{ url: 'https://instagram.com/frontroworbust' },
+{ url: 'https://www.tiktok.com/@frontroworbust' },
+{ url: 'https://linktr.ee/frontroworbust' },
+{ url: 'mailto:frontroworbust@gmail.com' }
+]
+const rules = [
+    
+]
+
+export { links, rules }
